1e3f4388de185c52de9011890cc485d81220407e
MouseEatsCat <descoteaux.michel@gmail.com>
2016-11-16 11:25:08 -0500

    Minor formatting changes

--- phpquicktranslate.php
+++ phpquicktranslate.php
@@ -7,7 +7,7 @@ GitHub: https://github.com/MouseEatsCat/phpquicktranslate
 
 include_once("config.php");
 
-if(!function_exists('_t')){
+if (!function_exists('_t')) {
 	function _t($string){
 		global $langParam;
 		global $useFirstString;
@@ -18,39 +18,39 @@ if(!function_exists('_t')){
 		$numOfLanguages = substr_count($string, '[:');
 
 		// Set the Default Language
-		if($numOfLanguages > 0){
+		if ($numOfLanguages > 0) {
 			// Find the first instance of a language section in the string
 			$tagstart = strpos($string, '[:') + 2;
 			$defaultLang = substr($string, $tagstart, strpos($string, ']') - $tagstart);
-		}else{
+		} else {
 			// Language was not present in string
 			return $useFirstString == true ? $string : "";
 		}
 
 		// Check to see if the "lang" GET parameter is set and is present in the string
-		if(!empty($getLang) && strpos($string, '[:'. $getLang .']') !== false){
+		if (!empty($getLang) && strpos($string, '[:'. $getLang .']') !== false) {
 			$defaultLang = $getLang;
-		}elseif(!empty($globalDefaultLang)){
+		} elseif (!empty($globalDefaultLang)) {
 			$defaultLang = $globalDefaultLang;
-			if($useFirstString == false){
+			if ($useFirstString == false) {
 				$defaultLang = $getLang;
 			}
-		}else{
-			if($useFirstString == false){
+		} else {
+			if ($useFirstString == false) {
 				$defaultLang = $getLang;
 			}
 		}
 		// CHECKING THE STRING
-		if(strpos($string, '[:'. $defaultLang .']') !== false){
+		if (strpos($string, '[:'. $defaultLang .']') !== false) {
 			$tmpString = substr($string, (strpos($string, '[:'. $defaultLang .']') + strlen($defaultLang) + 3));
-			if(strpos($tmpString, '[:') !== false){
+			if (strpos($tmpString, '[:') !== false) {
 				// Language was present in string and a following language section was found
 				return substr($tmpString, 0, strpos($tmpString, '[:'));
-			}else{
+			} else {
 				// Language was present in string and no further languages in string
 				return $tmpString;
 			}
-		}else{
+		} else {
 			// Language was not present in string
 			return $useFirstString == true ? $string : "";
 		}
@@ -60,7 +60,7 @@ if(!function_exists('_t')){
 ADDITIONAL FUNCTION
 Run the quick translate function and then echo the result
 */
-if(!function_exists('_et')){
+if (!function_exists('_et')) {
 	function _et($string){
 	  echo _t($string);
 	}

08a32a94c51a55f52585aed26a7ce18d00f5bccc
micheldmty <micheld@mtygroup.com>
2017-05-11 14:10:11 -0400

    Changed Structure

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # PHP Quick Translate
 
-A quick function for providing mutliple translations in PHP code.
+A quick function for providing multiple translations in PHP code.
 
 This script was developed as a personal project by [Michel Descoteaux](https://micheldescoteaux.com) and is free to use and distribute.
 
@@ -8,7 +8,7 @@ This script was developed as a personal project by [Michel Descoteaux](https://m
 
 ```php
 <?php
-	include_once('phpquicktranslate.php');
+	include_once('phpquicktranslate/phpquicktranslate.php');
 	_et('[:en]English Text[:fr]French Text');
 	// OR
 	echo _t('[:en]English Text[:fr]French Text');
@@ -27,10 +27,10 @@ French Text
 ## How to Set Up
 This script can be set up by using the following steps:
 
-1. Save the `phpquicktranslate.php` file into your web directory
+1. Save the `phpquicktranslate` folder into your web directory
 
-2. Add this code to the top of your php document: `<?php include_once('phpquicktranslate.php'); ?>`
+2. Add this code to the top of your php document: `<?php include_once('phpquicktranslate/phpquicktranslate.php'); ?>`
 
-3. The script will detect the language based on the "**lang**" `$_GET` parameter. So your url should look like this: `http://website.com/page.php?lang=en` (if no `lang` parameter is specified then the script will look for the first `[:]` section in the string and use that as the default)
+3. The script will detect the language based on the "**lang**" `$_GET` parameter. So your url should look similar to this: `http://website.com/page.php?lang=en` (if no `lang` parameter is specified then the script will look for the first `[:]` section in the string and use that as the default)
 
-4. Then in your php code if you run this function `<?php _et('[:en]Hola[:fr]Senior'); ?>`, the function will echo: `Hola`
+4. Then in your php code, if you run this function: `<?php _et('[:en]Hola[:fr]Senior'); ?>`, the function will echo: `Hola`
---
+++ phpquicktranslate/phpquicktranslate.php
@@ -0,0 +1,63 @@
+<?php
+/**
+PHP Quick Translate
+Author: Michel Descoteaux (https://micheldescoteaux.com)
+GitHub: https://github.com/MouseEatsCat/phpquicktranslate
+*/
+
+include_once("config.php");
+
+if (!function_exists('_t')) {
+	function _t($string){
+		global $langParam;
+		global $useFirstString;
+		global $globalDefaultLang;
+
+		// Set the Default Language
+		if (substr_count($string, '[:') > 0) {
+			// Find the first instance of a language section in the string
+			$tagstart = strpos($string, '[:') + 2;
+			$defaultLang = substr($string, $tagstart, strpos($string, ']') - $tagstart);
+		} else {
+			// Language was not present in string
+			return $useFirstString == true ? $string : "";
+		}
+
+		// Check to see if the "lang" GET parameter is set and is present in the string
+		if (!empty($langParam) && strpos($string, '[:'. $langParam .']') !== false) {
+			$defaultLang = $langParam;
+		} elseif (!empty($globalDefaultLang)) {
+			$defaultLang = $globalDefaultLang;
+			if ($useFirstString == false) {
+				$defaultLang = $langParam;
+			}
+		} else {
+			if ($useFirstString == false) {
+				$defaultLang = $langParam;
+			}
+		}
+		// CHECKING THE STRING
+		if (strpos($string, '[:'. $defaultLang .']') !== false) {
+			$tmpString = substr($string, (strpos($string, '[:'. $defaultLang .']') + strlen($defaultLang) + 3));
+			if (strpos($tmpString, '[:') !== false) {
+				// Language was present in string and a following language section was found
+				return substr($tmpString, 0, strpos($tmpString, '[:'));
+			} else {
+				// Language was present in string and no further languages in string
+				return $tmpString;
+			}
+		} else {
+			// Language was not present in string
+			return $useFirstString == true ? $string : "";
+		}
+	}
+}
+/**
+ADDITIONAL FUNCTION
+Run the quick translate function and then echo the result
+*/
+if (!function_exists('_et')) {
+	function _et($string){
+		echo _t($string);
+	}
+}
